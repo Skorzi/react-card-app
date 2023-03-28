3be578e75a63faecdd1c35a6e88206ba641beeb1
Skorzi <lobarev_danil@mail.ru>
2023-03-28 21:27:40 +0500

    подготовка для фильтра

---
+++ src/components/filter/Filter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Filter() {
+    return (
+        // при работе с фильтром - фильтруем список выдачи
+        <div className='filter'>
+            <div className='filter__block'>
+                <div className='filter__name'>
+                    <div className='filter__name_header'>Название</div>
+                    {/* элемент в который вложим список вариантов */}
+                </div>
+                <div className='filter__port'>
+                    <div className='filter__port_header'>Порт</div>
+                    {/* элемент в который вложим список вариантов */}
+                </div>
+                <div className='filter__type'>
+                    <div className='filter__type_header'>Тип</div>
+                    {/* элемент в который вложим список вариантов */}
+                </div>
+            </div>
+        </div>
+    )
+}
