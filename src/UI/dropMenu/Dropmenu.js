16041fb847f31c84315be7c1987aabfe3c5e4c33
Skorzi <lobarev_danil@mail.ru>
2023-03-30 17:35:41 +0500

    Стилизация чекбоксов, выбор для фильтрации по порту

--- src/UI/dropMenu/Dropmenu.js
+++ src/UI/dropMenu/Dropmenu.js
@@ -1,23 +1,38 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Checkbox, FormControlLabel, FormGroup } from '@mui/material';
 import {grey} from '@mui/material/colors';
 import '../../static/css/dropdown.css'
+import { BsCheck } from 'react-icons/bs';
 
 export default function Dropmenu({options}) {
     const [choisenArr, setChoisenArr] = useState([])
     const [countChoice, setCountChoice] = useState(0)
-    const ref = useRef(null)
-    function qwerty(){
-        console.log(ref.current)
+    let filterArray = []
+
+
+    function addOrRemoveOption(e){
+        if(e.checked){
+            setChoisenArr([...choisenArr, e.name])
+            setCountChoice(countChoice + 1)
+        }else{
+            filterArray = choisenArr.filter((option) => option !== e.name)
+            setChoisenArr(filterArray)
+            setCountChoice(countChoice - 1)
+        }
     }
-    //input type checkbox
+
+    useEffect(() => {
+        console.log(choisenArr)
+    }, [choisenArr])
+
     return (
         <div className='dropdown'>
             <div className='dropdown__input'>Выбрано {countChoice}</div>
             <div className='dropdown__menu'>
                 {options.map(option =>
-                    <div className='dropdown__option'>
-                        <div ref={ref} key={option.value} className='dropdown__name'>{option.name}</div>
+                    <div key={option.value} className='dropdown__option'>
+                        <input type="checkbox" id={option.value} name={option.name} onChange={e => addOrRemoveOption(e.target)}/>
+                        <label className='dropdown__name' for={option.value}>{option.name}</label>
                     </div>
                 )}
             </div>
